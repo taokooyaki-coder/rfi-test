a66219a368dcb7662f928064cb32e6e96cc0c59a
taokooyaki-coder <taokooyaki@gmail.com>
2026-04-22 11:30:36 +0700

    Update test.php

--- test.php
+++ test.php
@@ -1,1 +1,18 @@
-highlight_file("flag.php");
+<?php
+echo "<h3>--- MENGINTIP FOLDER RAHASIA ---</h3>";
+
+$dir = "You_Cant_Find_This_Dir";
+echo "Isi folder $dir : <br>";
+$files = scandir($dir);
+print_r($files);
+
+echo "<br><br><b>Mencoba membuka file di dalamnya...</b><br>";
+$target_file = $dir . "/flag.php";
+
+if (file_exists($target_file)) {
+    echo htmlspecialchars(file_get_contents($target_file));
+} else {
+    echo "Gak ada flag.php di dalam folder itu. Coba cek nama file lain di daftar array di atas.";
+}
+?>
+
